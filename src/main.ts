cfaa3719bd92192fd94de8928a466a76c11f88b2
“乃木流架” <1693145038@qq.com>
2023-10-24 00:17:21 +0900

    🎨 confetti放在一起，调整一些无关紧要的缩进

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,10 @@ import '@/style/index.css'
 import '@/style/nogiruka-logo.css'
 // ts
 import { initConfetti } from '@/utils/confetti.ts';
-
 document.addEventListener('click', (event: MouseEvent) => initConfetti(event));
+
+// todo 国际化
+// https://vue-i18n.intlify.dev/
 
 const app = createApp(App);
 app.use(router);
